f9b7aa088f01f6fec24f72ddb7a1d0ca40030a0e
Eric Pan <epan@users.noreply.github.com>
2017-04-07 10:41:42 -0700

    Use templates in templates

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-// TODO
+var App = () => (
+  <div>Some cliche salutation</div>
+);
+ReactDOM.render(<App />, document.getElementById("app"))
+
+var Milk = () => (
+  <li>milk</li>
+)
+var Eggs = () => (
+  <li>eggs</li>
+)
+
+var GroceryList = () => (
+  <ul>
+    <Milk />
+    <Eggs />
+  </ul>
+)
+
+ReactDOM.render(<GroceryList />, document.getElementById("grocerylist"))
